c47414a33ba7a942d3fc5b83b9575dd41d5c80e0
SteffenHub <steffenhub@users.noreply.github.com>
2024-06-06 17:15:26 +0200

    Translate Main file

--- src/Main.java
+++ src/Main.java
@@ -10,20 +10,20 @@ import java.util.ArrayList;
 public class Main {
 
     /**
-     * die Anzahl an Variablen Nummern im Regelwerk
+     * Number of variables in ruleset
      */
-    private static int anzahlVariablen;
+    private static int numberOfVariables;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        //stoppuhr starten
+        // save current time for calculation time
         Instant start = Instant.now();
 
-        //Konstanten
-        final String cnfDateiName = args[2];
-        final String ebrDateiName = args[3];
-        final String welcherAutoBauer = args[0];
-        final int anzahlZuBauendeModelle = Integer.parseInt(args[1]);
+        // Input constants
+        final String cnfFileName = args[2];
+        final String iRFileName = args[3];
+        final String procedure = args[0];
+        final int numberModelsToBuild = Integer.parseInt(args[1]);
         int dtcount = 0;
         for (int i = 0; i < args.length; i++) {
             if (args[i].equals("-dtcount")){
@@ -31,52 +31,51 @@ public class Main {
             }
         }
 
-        //das RegelWerk einlesen
-        ArrayList<int[]> cnfInt = getRegelwerk(cnfDateiName);
-        //die Einbauraten einlesen
-        double[] ebr = getEbr(ebrDateiName);
+        // Read the rule set
+        ArrayList<int[]> cnfInt = getRuleSet(cnfFileName);
+        // Read the installation rates
+        double[] ebr = TxtReaderWriter.getEbr(iRFileName);
 
-        switch (welcherAutoBauer) {
-            case "zufallsKonflikteMitNNF" -> new ZufallsKonflikteMitNNF(anzahlZuBauendeModelle, cnfInt, anzahlVariablen, ebr, cnfDateiName,dtcount);
-            case "allesZufaellig" -> new AllesZufaellig(anzahlZuBauendeModelle, cnfInt, anzahlVariablen, ebr, cnfDateiName);
-            case "allesZufaelligMitEbr01Raus" -> new alleZufaelligMitEbrWahlen01(anzahlZuBauendeModelle, cnfInt, anzahlVariablen, ebr, cnfDateiName);
+        switch (procedure) {
+            case "zufallsKonflikteMitNNF" -> new ZufallsKonflikteMitNNF(numberModelsToBuild, cnfInt, numberOfVariables, ebr, cnfFileName,dtcount);
+            case "allesZufaellig" -> new AllesZufaellig(numberModelsToBuild, cnfInt, numberOfVariables, ebr, cnfFileName);
+            case "allesZufaelligMitEbr01Raus" -> new alleZufaelligMitEbrWahlen01(numberModelsToBuild, cnfInt, numberOfVariables, ebr, cnfFileName);
             case "konsistenzPruefung" -> new KonsistenzPruefung(cnfInt, ebr);
-            case "untenNachOben" -> new untenNachOben(anzahlZuBauendeModelle, cnfInt, anzahlVariablen, ebr, cnfDateiName,dtcount);
-            case "EbrProzentualNaehern" -> new EbrProzentualNaehern(anzahlZuBauendeModelle, cnfInt, anzahlVariablen, ebr, cnfDateiName);
+            case "untenNachOben" -> new untenNachOben(numberModelsToBuild, cnfInt, numberOfVariables, ebr, cnfFileName,dtcount);
+            case "EbrProzentualNaehern" -> new EbrProzentualNaehern(numberModelsToBuild, cnfInt, numberOfVariables, ebr, cnfFileName);
             default -> System.err.println("Es ist nicht klar was der Autobauer tun soll. Waehle zwischen: zufallsKonflikteMitNNF, allesZufaellig, allesZufaelligMitEbr01Raus, konsistenzPruefung");
         }
 
-        //Stoppuhr Zeit ausgeben
+        // print calculation time
         Instant end = Instant.now();
         Duration interval = Duration.between(start, end);
         System.out.println("Execution time in seconds: " + interval.getSeconds());
     }
 
     /**
-     * liest das RegelWerk ein und formt es zu einer Liste aus int-Arrays um
+     * Reads the rule set and transforms it into a list of int arrays
      *
-     * @param cnfDateiName der Name der Txt-Datei indem die cnf ist
-     * @return das RegelWerk als Liste aus int-Arrays
+     * @param cnfFileName The name of the txt file with the CNF
+     * @return The rule set as a list of in arrays
      */
-    public static ArrayList<int[]> getRegelwerk(String cnfDateiName) {
-        //cnf einlesen
-        ArrayList<String> cnfStr = TxtReaderWriter.getTxtFromSamePath(cnfDateiName);
+    public static ArrayList<int[]> getRuleSet(String cnfFileName) throws Exception {
+        // read cnf
+        ArrayList<String> cnfStr = TxtReaderWriter.getTxtFromSamePath(cnfFileName);
 
-        String[] cnfStrSplit = cnfStr.get(0).split(" ");
-        String anzahlVarsStr = cnfStrSplit[cnfStrSplit.length - 2];
-        anzahlVariablen = Integer.parseInt(anzahlVarsStr);
-
-        //cnf zu int Arrays umformen
+        boolean foundNumberVars = false;
+        for (String line : cnfStr) {
+            if (line.charAt(0) == 'p') {
+                String[] cnfStrSplit = line.split(" ");
+                String numberVarsStr = cnfStrSplit[cnfStrSplit.length - 2];
+                numberOfVariables = Integer.parseInt(numberVarsStr);
+                foundNumberVars = true;
+                break;
+            }
+        }
+        if (!foundNumberVars){
+            throw new Exception("Can't found a line in cnf file starting with p");
+        }
+        // cnf to int arrays
         return Operation.stringListZuListAusIntArrays(cnfStr);
-    }
-
-    /**
-     * liest die Einbauraten ein
-     *
-     * @param ebrDateiName name der Datei, indem die Einbauraten stehen
-     * @return ein Array aus allen Einbauraten
-     */
-    public static double[] getEbr(String ebrDateiName) {
-        return TxtReaderWriter.getEbr(ebrDateiName);
     }
 }
